2c10d5baa30b0f42952f4a99c7ff5bdf9ce665df
Mochamad Gufron <mgufronefendi@gmail.com>
2017-01-17 10:27:46 +0700

    add hello file

---
+++ Hello.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import {View, Text, Image} from 'react-native';
+
+class Hello extends Component {
+    render() {
+        return (
+            <View><Text>SImple TExt</Text></View>
+        );
+    }
+}
