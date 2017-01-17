76832c4b65289d09625c759885f84224d9f4a82a
Mochamad Gufron <mgufronefendi@gmail.com>
2017-01-17 13:29:22 +0700

    add touchable opacity

--- Hello.js
+++ Hello.js
@@ -4,12 +4,14 @@ import {View, Text, Image} from 'react-native';
 class Hello extends Component {
     render() {
         return (
-            <View>
-              <Text>Simple Text
-              </Text>
-              <Text>This is simple test
-              </Text>
-            </View>
+            <TouchableOpacity>
+                <View>
+                  <Text>Simple Text
+                  </Text>
+                  <Text>This is simple test
+                  </Text>
+                </View>
+            </TouchableOpacity>
         );
     }
 }
